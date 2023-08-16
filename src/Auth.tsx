83ab58d0b2134f54b54632577288d15a4235a651
mscankat <m.salihcankat@gmail.com>
2023-08-16 20:47:30 +0300

    add: request auth token

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -1,3 +1,22 @@
+import { useSearchParams } from "react-router-dom";
+
 export default function Auth() {
+  const [searchParams] = useSearchParams();
+  const code = searchParams.get("code");
+  localStorage.setItem("code", code || "");
+  const postURL = new URL("http://localhost:3001/");
+  const body = { code: code };
+
+  fetch(postURL, {
+    method: "POST",
+    headers: {
+      Accept: "*/*",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": "true",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  }).then((data) => console.log(data.json()));
+
   return null;
 }
