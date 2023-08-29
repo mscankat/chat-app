cfdc4ef6b022210dddcc4b4439ccd7b858fa6e46
mscankat <m.salihcankat@gmail.com>
2023-08-29 19:06:18 +0300

    messages

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -1,33 +1,55 @@
-import { useState } from "react";
-
+interface messageType {
+  date?: number;
+  message: string;
+  user: string;
+  _id?: string;
+}
 export default function Chat() {
-  const [name, setName] = useState("");
   const token = localStorage.getItem("token");
   const userURL = "https://api.github.com/user";
-  fetch(userURL, {
-    headers: {
-      Authorization: "Bearer " + token,
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      setName(data.login);
-    });
+
+  function send(e: React.MouseEvent) {
+    e.preventDefault();
+  }
   return (
     <>
       <div className=" h-screen flex items-center justify-center bg-gray-950">
         <div className="bg-gray-900 h-4/5 w-3/6 flex flex-col rounded-lg">
-          <div className="flex-1 flex flex-col-reverse ">
+          <div className="flex-1 flex flex-col overflow-auto will-change-scroll">
             <div className="text-right m-8 p-2 px-3 ml-auto rounded-xl w-fit bg-slate-300">
               <div className="text-sm text-gray-700">{name}</div>
-              <div className="">this is a message</div>
+              <div className="">{isConnected}</div>
             </div>
-            <div className="text-left m-8 p-2 px-3 mr-auto rounded-xl w-fit bg-slate-300">
-              <div className="text-sm text-gray-700">{name}</div>
-              <div className="">this is also a message</div>
-            </div>
+            {messages.map((x) => {
+              if (name === x.user) {
+                return (
+                  <div
+                    key={x._id || x.user + x.message + x.date}
+                    className="text-right mx-8 my-2 p-1 px-3 ml-auto rounded-xl w-fit bg-slate-300"
+                  >
+                    <div className="text-sm text-gray-700">{x.user}</div>
+                    <div className="">{x.message}</div>
+                  </div>
+                );
+              } else {
+                return (
+                  <div
+                    key={x._id || x.user + x.message + x.date}
+                    className="text-left mx-8 my-2 p-1 px-3 mr-auto rounded-xl w-fit bg-slate-300"
+                  >
+                    <div className="text-sm text-gray-700">{x.user}</div>
+                    <div className="">{x.message}</div>
+                  </div>
+                );
+              }
+            })}
           </div>
-          <input className=" h-11 bg-slate-200 justify-end rounded-lg px-5 mx-2 mb-3" />
+          <input
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+            className=" h-11 bg-slate-200 justify-end rounded-lg px-5 mx-2 mb-3"
+          />
+          <button onClick={send}>Send</button>
         </div>
       </div>
     </>
