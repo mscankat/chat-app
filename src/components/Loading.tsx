4532ff39947940a12f0cce5192f0e546938c2cfa
mscankat <m.salihcankat@gmail.com>
2023-09-08 17:50:43 +0300

    create nextjs app

--- src/components/Loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex justify-center items-center h-screen flex-col gap-5 bg-gray-200">
-      <div className="w-16 h-16 border-t-4 border-blue-500 border-solid rounded-full animate-spin"></div>
-    </div>
-  );
-}
--- src/socket.js
+++
@@ -1,4 +0,0 @@
-import { io } from "socket.io-client";
-
-const URL = process.env.REACT_APP_SERVER_HOST;
-export const socket = io(URL);
