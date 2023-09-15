3ed60bd1fa236aef0d66cfafe43361d106cc9276
mscankat <m.salihcankat@gmail.com>
2023-09-15 20:07:54 +0300

    add:time

--- components/Message.tsx
+++ components/Message.tsx
@@ -24,8 +24,12 @@ export default function Message({
   } else {
     return (
       <div className="text-left mx-8 my-2 p-1 px-3 mr-auto rounded-xl w-fit bg-slate-300">
-        <div className="text-sm text-gray-700">{user}</div>
-        <div className="text-black">{message}</div>
+        <div className="flex items-baseline justify-between gap-2">
+          <div className="text-black">{message}</div>
+          <div className="text-xs text-gray-700 font-light ">
+            {new Date(date).getHours() + ":" + new Date(date).getMinutes()}
+          </div>
+        </div>
       </div>
     );
   }
